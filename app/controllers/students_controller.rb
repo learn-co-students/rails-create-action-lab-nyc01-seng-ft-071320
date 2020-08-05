7dd286134ce69c318b4458b110700dd81395262f
redazures <jwu_nyc@yahoo.com>
2020-08-04 21:39:14 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,17 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student=Student.new
   end
 
   def create
+    student=Student.create( first_name: params[:first_name],last_name:params[:last_name])
+
+    # @student.first_name=
+    # @student.last_name=
+    # @student.save
+    # byebug
+    redirect_to student
   end
 
 end
