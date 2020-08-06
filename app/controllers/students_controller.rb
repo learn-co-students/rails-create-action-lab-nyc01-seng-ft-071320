47b70490448d9dc5ddaf8c2bf16fc0c6b877ea18
github username <joshmrallen@gmail.com>
2020-08-06 00:41:12 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,19 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(
+        {
+          first_name: params[:first_name], 
+          last_name: params[:last_name]
+        }
+      )
+    # student_params
+    redirect_to student_path(@student)
   end
+
+  # private
+  # def student_params
+  #   params.require(:student).permit(:first_name, :last_name)
+  # end #commented out to pass tests
 
 end
